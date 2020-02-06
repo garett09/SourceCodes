c9339afef102e33e696b9b1c8b6c53a6e790958c
Karl <Karl@gov>
2020-02-06 07:04:59 +0700

    Incomplete processInformation Signed-off-by: adriansian

---
+++ Project2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+/**
+ *
+ * @author cisco
+ */
+public class Project2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HATDOG SI RUSSELL");
+        System.out.println("Art! Tibay");
+    }
+    
+}
---
+++ TestProject.java
@@ -0,0 +1,17 @@
+package project2;
+
+public class TestProject {
+
+	public static void main(String[] args) {
+
+		double screenSize;
+		int screenWidth;
+
+		System.out.println("Pangit ni Russell TITE");
+		System.out.println();
+
+		System.out.println("POGI KA BA?");
+
+	}
+
+}
