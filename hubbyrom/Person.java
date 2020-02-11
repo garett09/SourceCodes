1b54f096b7b43054791db5502ab5bb56dae8244d
Karl <Karladrian.solmiano.iics@ust.edu.ph>
2020-02-11 08:01:50 +0700

    Another object is Added P2.

--- hubbyrom/Person.java
+++ hubbyrom/Person.java
@@ -1,23 +1,31 @@
 package com.hubbyrom;
-
+import java.util.Scanner;
 public class Person {
 
-    private int age;
+    private int age,age1;
     private String name, gender;
     private double height;
 
     Person() {
-        age = 19;
+        Scanner input = new Scanner (System.in);
+         Scanner in = new Scanner (System.in);//other scanner for another input
+      //  System.out.println("How old are you?");
+        age = input.nextInt();
+        age1 = in.nextInt();//hindi need pero nandito lang dapat.
         gender = "Male";
         height = 5.10;
         name = "Adrian Garett Sian";
         System.out.println("Person object was created and initialized ");
+        System.out.println("This person is " + this.age + " years old.");
     }
     public String getName (){ //getter
         return this.name;
         
     }
-    public void setName(String newName){ //setter
+    public int getAge(){ //this gets the age of the this.age
+        return this.age;
+    }
+    public void setName(String newName){ //setter. this sets for a new name in the name method
         this.name= newName;
     }
     void sleep(double duration) {
@@ -26,6 +34,6 @@ public class Person {
     }
 
     void walk(double distance) {
-        System.out.println(name + " can walk " + distance + " kilometers per day.");
+        System.out.println("He" + " can walk " + distance + " kilometers per day.");
     }
 }
--- hubbyrom/Test.java
+++ hubbyrom/Test.java
@@ -8,7 +8,12 @@ public class Test {
     public static void main(String[] args) {
         Person p1 = new Person();
         System.out.println(p1.getName());
+        System.out.println(p1.getAge());
         p1.walk(3.7);
+        
+        Person p2 = new Person();
+        p2.setName("Pedro Sian");
+        System.out.println(p2.getName());
     }
 
 }
