57e6e297c60047ecc1dc0aa69c189d807f4f1bfc
Adrian Garett Sian <57400082+adriansian@users.noreply.github.com>
2020-02-16 12:41:19 +0800

    Minor changes. Removed computation method.

--- bodymassindex/BMI.java
+++ bodymassindex/BMI.java
@@ -2,26 +2,25 @@ package BMI;
 
 import java.util.Scanner;
 
-public class BMI_Computation {
+public class BMI_InfoSheet {
 
     private String name;
     private int age;
     private double height, weight;
 
-    BMI() {
-        System.out.println("Please input height in meters and weight in KG");
-        System.out.println();
-
+    public BMI_InfoSheet() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Please input height in meters and weight in KG");
+
         System.out.println("Name?");
-        name = input.next();
+        name = input.nextLine();
         System.out.println("Age?");
         age = input.nextInt();
         System.out.println("Height in Meters");
         height = input.nextDouble();
         System.out.println("Weight in KG");
         weight = input.nextDouble();
-        input.close();
+        
 
     }
 
@@ -42,28 +41,10 @@ public class BMI_Computation {
         return this.height;
     }
 
-    void computation() {
-        double heightSquared = height * height;
-        double comp = this.weight / heightSquared;
-        double rounded = Math.round(comp * 1000) / 1000;
-        System.out.print("\t     " +"Your BMI is " + rounded);
-        if (rounded <= 18.5) {
-            System.out.print(" You are underweight");
-        } else if (rounded >= 18.5 && rounded <= 24.9) {
-            System.out.print(" You are normal!");
-
-        } else if (rounded >= 25 && rounded <= 29.9) {
-            System.out.print(" You are overweight!");
-        } else if (rounded >= 30) {
-            System.out.print(" You are obese!");
-        } else {
-            System.out.print(" Wrong information!");
-        }
-    }
-
-    void display() {
-        computation();
-    }
+  public void display() {
+		BMI_Computation bmi = new BMI_Computation();
+		bmi.computation();
+	}
 }
 
     
