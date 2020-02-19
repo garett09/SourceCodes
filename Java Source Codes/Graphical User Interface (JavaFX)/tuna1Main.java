bde17bc7573fac54bee1b8b9a52eed7f9d59274f
Adrian Garett Sian <57400082+adriansian@users.noreply.github.com>
2020-02-19 22:43:30 +0800

    GUI with JFrame and Event Handling

---
+++ Java Source Codes/Graphical User Interface (JavaFX)/tuna1Main.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+class tuna1Main {
+
+	public static void main(String[] args) {
+
+		tuna1 tuna = new tuna1();
+
+		tuna.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		tuna.setSize(300, 300);
+		tuna.setVisible(true);
+
+	}
+
+}
