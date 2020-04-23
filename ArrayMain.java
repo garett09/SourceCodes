9b951a79acfd7f36d691d3986115736558824561
Adrian Garett Sian <57400082+adriansian@users.noreply.github.com>
2020-04-23 16:45:32 +0800

    Updated ArrayMain

--- ArrayMain.java
+++ ArrayMain.java
@@ -25,7 +25,16 @@ public class ArrayMain {
 		}
 		
 		//COPYING ARRAYS
-		
+			int original[] = {1,2,3,4,5};
+		int temporary [] = {10,9,8,7,6,5,4,3,2,1};
+		System.arraycopy(original,0,temporary,0,original.length);
+		//original file, start of the index (orig) , file that will be copied INTO, start where it will be replaced,LENGHT of the array that will be copied from the ORIGINAL to the TEMPORARY
+		original=temporary;
+		temporary=null;
+		System.out.println("Printing the new array");
+		for(int new1 : original) {
+			System.out.print(new1);
+		}
 	
 	
 	}
