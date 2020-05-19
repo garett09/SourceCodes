93573414292df78aef6c65fd4573f121c9a7d590
Adrian Sian <adriansian@gmail.com>
2020-05-19 14:09:50 +0800

    changed video

--- Abby's birthday/js/index.js
+++ Abby's birthday/js/index.js
@@ -296,7 +296,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/gbICivOO26U?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/OoPf6Ds-GpU");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
