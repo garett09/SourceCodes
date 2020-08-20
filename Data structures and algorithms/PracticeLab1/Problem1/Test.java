8da85ab05f68101c2715e3304ce32ff8117998bb
Adrian Garett Sian <adriansian@gmail.com>
2020-08-20 09:01:55 +0800

    sourcecode: add Lab1

---
+++ Data structures and algorithms/PracticeLab1/Problem1/Test.java
@@ -0,0 +1,15 @@
+ import java.util.*;
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList <Integer> testInts = new ArrayList();
+		testInts.add(5);
+		testInts.add(6);
+		testInts.add(7);
+		testInts.add(20);
+		testInts.add(40);
+	
+		System.out.println(Integers.computeSum(testInts));
+	}
+
+}
