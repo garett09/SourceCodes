b4db7fa812f9665773ddb175aab392619f531723
Adrian Sian <adriansian@gmail.com>
2020-10-06 07:25:08 +0800

    queues: modified isFull() and isEmpty()
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- Data structures and algorithms/Queues/sourceCode/Queue.java
+++ Data structures and algorithms/Queues/sourceCode/Queue.java
@@ -1,16 +1,16 @@
 public class Queue {
 
-	int queue[] = new int[5];
+	int queue[] = new int[10];
 	int size;
 	int front;
 	int rear;
 
 	public void enQueue(int data) {
-		if(!isFull()) {
-		queue[rear] = data;
-		rear = (rear + 1) % 5;
-		size++;
-		}else {
+		if (!isFull()) {
+			queue[rear] = data;
+			rear = (rear + 1) % queue.length;
+			size++;
+		} else {
 			System.out.println("The queue is full");
 		}
 
@@ -18,10 +18,10 @@ public class Queue {
 
 	public int deQueue() {
 		int data = queue[front];
-		if(!isEmpty()) {
-		front = (front + 1) % 5;
-		size--;
-		}else {
+		if (!isEmpty()) {
+			front = (front + 1) % queue.length;
+			size--;
+		} else {
 			System.out.println("Queue is empty");
 		}
 		return data;
@@ -30,7 +30,7 @@ public class Queue {
 	public void show() {
 		System.out.print("Elements:");
 		for (int i = 0; i < size; i++) {
-			System.out.print(queue[(front + i) % 5] + " ");
+			System.out.print(queue[(front + i) % queue.length] + " ");
 		}
 		System.out.println();
 		System.out.print("Elements in the Array: ");
@@ -45,13 +45,21 @@ public class Queue {
 
 	public boolean isEmpty() {
 
-		return size == 0;
+		if (front == (rear + 1) % queue.length) {
+			return true;
+		} else {
+			return false;
+		}
 
 	}
-	
+
 	public boolean isFull() {
 
-		return size == 5;
+		if (rear == queue.length - 1) {
+			return true;
+		} else {
+			return false;
+		}
 
 	}
 }
